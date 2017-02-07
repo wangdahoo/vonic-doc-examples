0e02afe8848368a0ff6c8698892b010b29eeed57
wangdahoo <157195705@qq.com>
2017-02-07 23:57:30 +0800

    Input/Radio/Search

--- src/main.js
+++ src/main.js
@@ -4,11 +4,15 @@ import Vonic from 'vonic/src/index.js'
 // Pages
 import ComponentAndService from './components/ComponentAndService.vue'
 import MdButton from './components/MdButton.vue'
+import Input from './components/Input.vue'
+import Search from './components/Search.vue'
 
 // Routes
 const routes = [
   { path: '/componentAndService', component: ComponentAndService },
-  { path: '/mdButton', component: MdButton }
+  { path: '/mdButton', component: MdButton },
+  { path: '/input', component: Input },
+  { path: '/search', component: Search },
 ]
 
 Vue.use(Vonic.app, {
